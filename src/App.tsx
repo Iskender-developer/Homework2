9471896f98f6fa980cfad471b66c5a638310b597
Iskender-developer <iskashukur@gmail.com>
2026-06-15 22:51:11 +0500

    testSecond

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./index.css"
 export default function App() {
   return (
     <div>
-      <h1>Hello world!</h1>
+      <h1 className="text-8xl font-bold underline-offset-4">Hello world!</h1>
     </div>
   )
 }
