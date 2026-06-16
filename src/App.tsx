0942c0dc32dabd7fc6430fbf7845c33a4c409888
Iskender-developer <iskashukur@gmail.com>
2026-06-16 11:18:34 +0500

    finish1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import "./index.css"
+import "./index.css";
+import First from "./First";
 
 export default function App() {
   return (
-    <div>
-      <h1 className="text-8xl font-bold underline-offset-4">Hello world!</h1>
-    </div>
-  )
+    <>
+      <First />
+    </>
+  );
 }
---
+++ src/First.tsx
@@ -0,0 +1,39 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+
+const schemaO = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.string().email("Invalid email"),
+  password: z.string().min(8, "weak password"),
+});
+
+type schemaO = z.infer<typeof schemaO>;
+
+export default function First() {
+  const { register, handleSubmit } = useForm<schemaO>({
+    resolver: zodResolver(schemaO),
+  });
+
+  const onSubmit = async (data: schemaO) => {
+    try {
+      await axios.post("https://jsonplaceholder.typicode.com/users", data);
+      alert("success");
+    } catch (error) {
+      alert("error");
+    }
+  };
+
+  return (
+    <form className="flex flex-wrap w-70 p-3 justify-between border" onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="1">Name: </label>
+      <input type="text" {...register("name")} id="1" className="border rounded-md"/>
+      <label htmlFor="2">Email: </label>
+      <input type="email" {...register("email")} id="2" className="border rounded-md"/>
+      <label htmlFor="3">Password: </label>
+      <input type="password" {...register("password")} id="3" className="border rounded-md"/>
+      <button type="submit" className="border px-2 bg-amber-300 ">Submit</button>
+    </form>
+  );
+}
